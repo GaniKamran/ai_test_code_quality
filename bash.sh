790036b4f203e7a3d4b7b2b2695005e0d4ce1543
GaniKamran <92980355+GaniKamran@users.noreply.github.com>
2026-06-02 16:21:36 +0400

    Re-enable test execution in bash script

--- bash.sh
+++ bash.sh
@@ -7,10 +7,13 @@ export PYTHONPATH
 echo "=== [1/3] DB setup ==="
 python setup_db.py
 
-echo ""
-echo "=== [2/3] Running tests ==="
-python -m pytest test/ -v --tb=short -s
+
 
 echo ""
 echo "=== [3/3] Starting app ==="
 exec python -m uvicorn train_station_app.app:app --host 0.0.0.0 --port 8000
+
+
+echo ""
+echo "=== [2/3] Running tests ==="
+python -m pytest test/ -v --tb=short -s
